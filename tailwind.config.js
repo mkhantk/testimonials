d0a4494b41d50ae776aca0791b7503de35a43467
Min Khant Kyaw <mkhantk102@gmail.com>
2024-07-10 15:14:15 +0630

    Add custom font and style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ModerateViolet': 'hsl(263, 55%, 52%)',
+        'VeryDarkGrayishBlue': 'hsl(217, 19%, 35%)',
+        'VeryDarkBlackishBlue': 'hsl(219, 29%, 14%)',
+        'WhitE': 'hsl(0, 0%, 100%)',
+        'LightGrayishBlue': 'hsl(210, 46%, 95%)',
+        'LightGray': 'hsl(0, 0%, 81%)',
+      },
+      fontFamily: {
+        body: ['Barlow Semi Condensed']
+      }
+    },
   },
   plugins: [],
 }
